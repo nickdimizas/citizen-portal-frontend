b0d8ee0bc71d95a237fb9a37cc513f09df845206
Nick Dimizas <nikkosdim@gmail.com>
2025-08-31 00:07:57 +0300

    docs: update README

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { store } from './store/store';
+import { store } from '@/store/store';
 
 import './index.css';
 
